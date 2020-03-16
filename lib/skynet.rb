cdffe151040d1024fca44b4806b7e482e4631568
Beyarz <beyar-123@hotmail.com>
2020-03-16 22:42:20 +0100

    Added linter

--- lib/skynet.rb
+++ lib/skynet.rb
@@ -4,20 +4,21 @@ require 'net/http'
 require 'net/https'
 require 'httparty'
 
+# The main class for the SDK
 class Skynet
-  def uri_skynet_prefix()
+  def uri_skynet_prefix
     'sia://'
   end
 
   def strip_prefix(str)
-    if str.index(self.uri_skynet_prefix()) == 0
-      str.delete_prefix(self.uri_skynet_prefix())
+    if str.index(uri_skynet_prefix).zero?
+      str.delete_prefix(uri_skynet_prefix)
     else
       str
     end
   end
 
-  def default_upload_options()
+  def default_upload_options
     {
       portal_url: 'https://siasky.net',
       portal_upload_path: 'skynet/skyfile',
@@ -27,51 +28,46 @@ class Skynet
     }
   end
 
-  def default_download_options()
+  def default_download_options
     {
-      portal_url: 'https://siasky.net',
+      portal_url: 'https://siasky.net'
     }
   end
 
   def upload_file_request(path, options = nil)
-    if options.nil?
-      options = default_upload_options()
-    end
+    options = default_upload_options if options.nil?
 
     file = File.open(path, 'rb')
+    file.close
     host = options[:portal_url]
     path = options[:portal_upload_path]
     url = "#{host}/#{path}"
 
     req = HTTParty.post(url, options[:portal_file_fieldname])
-    return req
+    req
   end
 
   def upload_file(path, options = nil)
-    self.uri_skynet_prefix + self.upload_file_request(path, options).to_json
+    uri_skynet_prefix + upload_file_request(path, options).to_json
   end
 
   def upload_file_request_with_chunks(path, options = nil)
-    if options.nil?
-      options = self.default_upload_options()
-    end
+    options = default_upload_options if options.nil?
 
-    filename = options[:custom_filename] ? options[:custom_filename] : path
-    req = HTTParty.post("#{options[:portal_url]}/#{options[:portal_upload_path]}?filename=#{filename}")
+    file = options[:custom_filename] || path
+    req = HTTParty.post(
+      "#{options[:portal_url]}/#{options[:portal_upload_path]}?filename=#{file}"
+    )
+    req
   end
 
-  def upload_directory(path, options = nil)
-  end
+  def upload_directory(path, options = nil); end
 
-  def upload_directory_request(path, options = nil)
-  end
+  def upload_directory_request(path, options = nil); end
 
-  def download_file(path, skylink, options = nil)
-  end
+  def download_file(path, skylink, options = nil); end
 
-  def download_file_request(skylink, options = nil, stream = false)
-  end
+  def download_file_request(skylink, options = nil, stream = false); end
 
-  def walk_directory(path)
-  end
+  def walk_directory(path); end
 end
